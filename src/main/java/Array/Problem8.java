f18541d891b282748669e76a423be6e49d4397e8
Anshul0506 <160764102+Anshul0506@users.noreply.github.com>
2024-03-09 15:53:43 +0000

    Find out the occurrence of an integer in the array

---
+++ src/main/java/Array/Problem8.java
@@ -0,0 +1,20 @@
+// Find out the occurrence of an integer in the array 
+
+class Problem8
+{
+  public static void main(String[]args)
+  {
+    int a[]= {1,1,2,5,3,2,5,6,3,7,6,9,5,8,5,6,6};
+    int l=a.length;
+    int x=5;
+    int r=0;
+    for(int i=0;i<l;i++)
+    {
+      if(x==a[i])
+      {
+        r=r+1;
+      }
+    }
+    System.out.println("The Number "+x+" is repeating "+r+" time in array");
+  }
+}
